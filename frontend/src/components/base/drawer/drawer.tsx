fea9fb943408bc6eb211da120da0b587d568d100
oliUmbi <oliver.umbricht@hispeed.ch>
2024-06-09 14:16:09 +0200

    updated drawer

--- frontend/src/components/base/drawer/drawer.tsx
+++ frontend/src/components/base/drawer/drawer.tsx
@@ -3,32 +3,18 @@ import {ReactNode, useEffect, useRef} from "react";
 
 export interface DialogProps {
     children: ReactNode,
-    modal: boolean,
     open: boolean
 }
 
 const Drawer = (props: DialogProps) => {
 
-    const dialog = useRef<HTMLDialogElement | null>(null)
-
-    useEffect(() => {
-        if (dialog.current) {
-            if (props.open) {
-                if (props.modal) {
-                    dialog.current.showModal()
-                } else {
-                    dialog.current.show()
-                }
-            } else {
-                dialog.current.close()
-            }
-        }
-    }, [props.open, props.modal]);
-
     return (
-        <dialog className="drawer" ref={dialog}>
-            {props.children}
-        </dialog>
+        <div className={"drawer " + (props.open ? "open" : "")}>
+            <div className="drawer__backdrop"></div>
+            <div className="drawer__content">
+                {props.children}
+            </div>
+        </div>
     )
 }
 
