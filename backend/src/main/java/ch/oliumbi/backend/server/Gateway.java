86451ebc94c5a6d0a74d850f26d5cf84e0f5b510
oliUmbi <oliver.umbricht@hispeed.ch>
2024-08-07 00:01:36 +0200

    some refactoring

--- backend/src/main/java/ch/oliumbi/backend/server/Gateway.java
+++ backend/src/main/java/ch/oliumbi/backend/server/Gateway.java
@@ -1,6 +1,12 @@
 package ch.oliumbi.backend.server;
 
+import ch.oliumbi.backend.autoload.Autoload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.nio.file.PathMatcher;
+import java.util.List;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
@@ -8,15 +14,50 @@ import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 import org.eclipse.jetty.util.BufferUtil;
 import org.eclipse.jetty.util.Callback;
 
+@Autoload
 public class Gateway extends Handler.Abstract {
+
+  private final List<Endpoint<?, ?>> endpoints;
+
+  public Gateway(Endpoint<?, ?>[] endpoints) {
+    this.endpoints = List.of(endpoints);
+  }
 
   @Override
   public boolean handle(Request request, Response response, Callback callback) throws Exception {
 
-    request.getHttpURI()
+    System.out.println(request.getHttpURI().asString());
+    System.out.println(request.getHttpURI().getCanonicalPath());
+    System.out.println(request.getHttpURI().getDecodedPath());
 
+    // method, url, body
     response.write(true, BufferUtil.toBuffer("test"), callback);
 
+    for (Endpoint<?, ?> endpoint : endpoints) {
+      Type[] genericInterfaces = endpoint.getClass().getGenericInterfaces();
+
+      if (genericInterfaces.length > 0 && genericInterfaces[0] instanceof ParameterizedType parameterizedType && parameterizedType.getRawType() == Endpoint.class) {
+
+        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+
+        if (actualTypeArguments.length != 2) {
+          throw new RuntimeException("Failed to handle endpoint, reason: too many generic types");
+        }
+
+        Type responseType = actualTypeArguments[0];
+        Type requestType = actualTypeArguments[1];
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeFactory typeFactory = objectMapper.getTypeFactory();
+
+        Object o = objectMapper.readValue("{\"username\": \"test\", \"password\": \"test\"}",
+            typeFactory.constructType(requestType));
+
+        System.out.println(o);
+
+        endpoint.handle(new ch.oliumbi.backend.server.Request<>(o));
+      }
+    }
 
     return true;
   }
--- backend/src/main/java/ch/oliumbi/backend/server/Server.java
+++ backend/src/main/java/ch/oliumbi/backend/server/Server.java
@@ -2,74 +2,36 @@ package ch.oliumbi.backend.server;
 
 import ch.oliumbi.backend.autoload.Autoload;
 import ch.oliumbi.backend.confguration.Configuration;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import java.io.ObjectInputStream;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.List;
 import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.ServerConnector;
 
 @Autoload
 public class Server {
 
   private final Configuration configuration;
-  private final List<Endpoint<?, ?>> endpoints;
+  private final Gateway gateway;
 
-  public Server(Configuration configuration, Endpoint<?, ?>[] endpoints) throws Exception {
+  public Server(Configuration configuration, Gateway gateway) {
     this.configuration = configuration;
-    this.endpoints = List.of(endpoints);
+    this.gateway = gateway;
 
-    for (Endpoint<?, ?> endpoint : endpoints) {
-      Type[] genericInterfaces = endpoint.getClass().getGenericInterfaces();
-
-      if (genericInterfaces.length > 0 && genericInterfaces[0] instanceof ParameterizedType parameterizedType && parameterizedType.getRawType() == Endpoint.class) {
-
-        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
-
-        if (actualTypeArguments.length != 2) {
-          throw new RuntimeException("Failed to handle endpoint, reason: too many generic types");
-        }
-
-        Type responseType = actualTypeArguments[0];
-        Type requestType = actualTypeArguments[1];
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        TypeFactory typeFactory = objectMapper.getTypeFactory();
-
-        Object o = objectMapper.readValue("{\"username\": \"test\", \"password\": \"test\"}",
-            typeFactory.constructType(requestType));
-
-        System.out.println(o);
-
-        endpoint.handle(new Request<>(o));
-      }
-    }
-
+    start();
   }
 
   public void start() {
 
     org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server();
-    Connector connector = new ServerConnector(server);
-    server.addConnector(connector);
+    ServerConnector serverConnector = new ServerConnector(server);
+    serverConnector.setPort(configuration.integer("server.port"));
+    server.addConnector(serverConnector);
 
-    server.setHandler(new Handler.Abstract()
-    {
-      @Override
-      public boolean handle(Request request, Response response, Callback callback)
-      {
-        // Succeed the callback to signal that the
-        // request/response processing is complete.
-        callback.succeeded();
-        return true;
-      }
-    });
+    server.setHandler(gateway);
 
-    server.start();
+    try {
+      server.start();
+    } catch (Exception e) {
+      // todo
+      throw new RuntimeException(e);
+    }
   }
-
 }
