466db13dd273272d4f697469b8efed4719c0e37e
oliUmbi <oliver.umbricht@competec.ch>
2024-09-10 17:28:17 +0200

    removed comments

--- migration/V03-jublawoma-donation-data.sql
+++ migration/V03-jublawoma-donation-data.sql
@@ -1,26 +1,3 @@
--- TRUNCATE TABLE account CASCADE;
--- INSERT INTO account(account_id, firstname, lastname, email, password)
--- VALUES ('aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'Alex', 'Yeet', 'email1', '$2a$10$N75JmlVwH3214SKi/ipxIOvHKCG.WW0uTCVJlbDImlqqk/7VQlua6'),
---        ('aaaaaaa1-bbb2-cccc-dddd-eeeeeeeeeeee', 'Bob', 'Burger', 'email2', '$2a$10$N75JmlVwH3214SKi/ipxIOvHKCG.WW0uTCVJlbDImlqqk/7VQlua6'),
---        ('aaaaaaa1-bbb3-cccc-dddd-eeeeeeeeeeee', 'Carlo', 'Pierotto', 'email3', '$2a$10$N75JmlVwH3214SKi/ipxIOvHKCG.WW0uTCVJlbDImlqqk/7VQlua6');
---
--- TRUNCATE TABLE account_session CASCADE;
--- INSERT INTO account_session(account_session_id, account_id, token, expires)
--- VALUES ('aaaaaaa2-bbb1-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'token1', current_timestamp),
---        ('aaaaaaa2-bbb2-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'token2', current_timestamp),
---        ('aaaaaaa2-bbb3-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'token3', current_timestamp),
---        ('aaaaaaa2-bbb4-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb2-cccc-dddd-eeeeeeeeeeee', 'token4', current_timestamp),
---        ('aaaaaaa2-bbb5-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb2-cccc-dddd-eeeeeeeeeeee', 'token5', current_timestamp);
---
--- TRUNCATE TABLE account_permission CASCADE;
--- INSERT INTO account_permission(account_permission_id, account_id, permission)
--- VALUES ('aaaaaaa3-bbb1-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'JUBLAWOMA_ADMIN'),
---        ('aaaaaaa3-bbb2-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'UNCLET_ADMIN'),
---        ('aaaaaaa3-bbb3-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb3-cccc-dddd-eeeeeeeeeeee', 'JUBLAWOMA_ADMIN'),
---        ('aaaaaaa3-bbb4-cccc-dddd-eeeeeeeeeeee', 'aaaaaaa1-bbb3-cccc-dddd-eeeeeeeeeeee', 'UNCLET_ADMIN');
-
-
-
 TRUNCATE TABLE jublawoma_donation CASCADE;
 INSERT INTO jublawoma_donation(id, title, description, contact, start, finish)
 VALUES ('aaaaaaa1-bbb1-cccc-dddd-eeeeeeeeeeee', 'Hela 2024', 'Wir sind auf Ihre Unterstützung angewiesen, damit wir im Lager leckeres und gesundes Essen zu servieren können. Jede Spende hilft uns, einen vielfältigeren Menuplan zu erstellen. Die Spenden sammeln wir bei der Gepäckabgabe. ', 'Bei Fragen oder Problemen können Sie Oliver Umbricht auf Whatsapp [078 850 04 73] erreichen.', '2024-09-08', '2024-09-22');
