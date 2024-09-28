8f4dc40cc992280d02e82bb955c2047b8a64d705
oliUmbi <oliver.umbricht@hispeed.ch>
2024-09-28 09:34:39 +0200

    updated action

--- ui/src/sites/jublawoma/admin/jublawoma-admin-home.tsx
+++ ui/src/sites/jublawoma/admin/jublawoma-admin-home.tsx
@@ -1,135 +1,14 @@
 import Flex from "../../../components/flex/flex";
-import Markdown from "../../../components/markdown/markdown";
-import useApi from "../../../hooks/use-api";
-import {Enviroment} from "../../../enums/shared/enviroment";
-import useInput from "../../../hooks/use-input";
-import InputFile from "../../../components/input/file/input-file";
-import {useEffect} from "react";
-import Button from "../../../components/button/button";
-import Picture from "../../../components/picture/picture";
-import {configuration} from "../../../services/configuration";
-import {MarkdownItem} from "../../../interfaces/shared/markdown";
+import Text from "../../../components/text/text";
 
 const JublawomaAdminHome = () => {
 
-  const api = useApi<File>(Enviroment.JUBLAWOMA_ADMIN, "POST", "/image")
-
-  const input = useInput<File>(true)
-
-  const upload = () => {
-    if (!input.valid) {
-      return
-    }
-
-    api.execute({
-      body: input.value
-    })
-  }
-
-  useEffect(() => {
-    console.log(input)
-  }, [input]);
-
-  useEffect(() => {
-    console.log(api)
-  }, [api]);
-
-  const data: MarkdownItem[] = [
-    {
-      type: "heading-1",
-      value: "Titel",
-      children: []
-    },
-    {
-      type: "heading-2",
-      value: "Titel 2",
-      children: []
-    },
-    {
-      type: "heading-3",
-      value: "Titel 3",
-      children: []
-    },
-    {
-      type: "paragraph",
-      value: "Text",
-      children: []
-    },
-    {
-      type: "button",
-      value: "Mehr erfahren|https://jublawoma.ch",
-      children: []
-    },
-    {
-      type: "image",
-      value: "fbeca530-3850-43e7-834e-3e50a9987ac2",
-      children: []
-    },
-    {
-      type: "grid",
-      value: 3,
-      children: [
-        {
-          type: "heading-1",
-          value: "Titel",
-          children: []
-        },
-        {
-          type: "heading-2",
-          value: "Titel 2",
-          children: []
-        },
-        {
-          type: "heading-3",
-          value: "Titel 3",
-          children: []
-        },
-        {
-          type: "flex",
-          value: null,
-          children: [
-            {
-              type: "paragraph",
-              value: "Text",
-              children: []
-            },
-            {
-              type: "image",
-              value: "fbeca530-3850-43e7-834e-3e50a9987ac2",
-              children: []
-            }
-          ]
-        },
-        {
-          type: "button",
-          value: "Mehr erfahren|https://jublawoma.ch",
-          children: []
-        },
-        {
-          type: "image",
-          value: "fbeca530-3850-43e7-834e-3e50a9987ac2",
-          children: []
-        },
-        {
-          type: "image",
-          value: "fbeca530-3850-43e7-834e-3e50a9987ac2",
-          children: []
-        },
-        {
-          type: "image",
-          value: "fbeca530-3850-43e7-834e-3e50a9987ac2",
-          children: []
-        },
-      ]
-    }
-  ]
 
   return (
-      <Flex xl={{widthMax: "xl", direction: "column", gap: 8}}>
-        <InputFile {...input} label="Image"/>
-        <Button onClick={upload} highlight={true}>Upload</Button>
-        <Picture api={configuration.api.jublawoma} id="fbeca530-3850-43e7-834e-3e50a9987ac2" alt="image" side="width" rounded={true}/>
-        <Markdown markdown={data} api={configuration.api.jublawoma}/>
+      <Flex xl={{direction: "column", align: "center"}}>
+        <Flex xl={{widthMax: "l", width: true, direction: "column", gap: 4}}>
+          <Text type="h1">Home</Text>
+        </Flex>
       </Flex>
   )
 }
